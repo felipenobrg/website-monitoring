2b513d7038b8bd48d45c9f6424d1310fe37395d8
felipenobrg <felipenobrega2012@gmail.com>
2024-09-02 19:25:22 -0300

    create monitoring func

--- hello.go
+++ hello.go
@@ -1,24 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	showIntro()
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
 
-	command := readCommand()
-
-	switch command {
-	case 1:
-		fmt.Println("Iniciando Monitoramento...")
-	case 2:
-		fmt.Println("Exibindo Logs...")
-	case 0:
-		fmt.Println("Saindo do Programa...")
-	default:
-		fmt.Println("Não foi executado nenhum comando.")
+	for {
+		showMenu()
+		command := readCommand()
+		switch command {
+		case 1:
+			initMonitoring()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do Programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não foi executado nenhum comando.")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -30,6 +34,12 @@ func showIntro() {
 	fmt.Println("Este programa está na versão", versao)
 }
 
+func showMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
 func readCommand() int {
 	var command int
 	fmt.Scan(&command)
@@ -37,4 +47,19 @@ func readCommand() int {
 	fmt.Println("O valor da variável comando é:", command)
 
 	return command
+}
+
+func initMonitoring() {
+	fmt.Println("Monitorando...")
+	sites := []string{"https://www.alura.com.br", "https://renovecasajp.com"}
+
+	for i, site := range sites {
+		fmt.Println("Passando na posição", i, "o site", site)
+	}
+
+}
+
+func showNames() {
+	names := []string{"Douglas", "Maria", "João"}
+	fmt.Println(names)
 }
